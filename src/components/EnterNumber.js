3243d57334065016276ef029ebb00d988d205b45
Nick Neiert <nickster7421@gmail.com>
2019-01-16 16:28:19 -0600

    completed decreaseNumber method

---
+++ src/components/EnterNumber.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class EnterNumber extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            incrementNumber: '',
+                }
+    }
+
+onIncrementNumber = (event)=> {
+    this.setState({
+        incrementNumber: event.target.value,
+    })
+}
+decreaseNumber = (event)=>{
+    this.setState({
+        incrementNumber: this.state.incrementNumber - this.state.incrementNumber
+    })
+}
+    render(){
+        console.log(this.state);
+        return(
+            <div>
+            <button>Increase</button>
+            <input onChange={this.onIncrementNumber} type="text" placeholder="enter increment Number" />
+            <button onClick={this.decreaseNumber}>Decrease</button>
+            {JSON.stringify(this.state.incrementNumber)}
+            </div>
+            
+        )
+        
+    }
+    
+}
+export default EnterNumber;
